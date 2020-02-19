94c0b1090755517505c5f9c4c6297c118e0de590
andretodee <andretodee@gmail.com>
2020-02-19 18:25:12 +0100

    day 1

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.public')
+
+@section('content')
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <h1>Lista di tutti i post</h1>
+                <ul>
+                    @forelse ($posts as $post)
+                        <li>{{ $post->title }}</li>
+
+                    @empty
+                        <li>non ci sono ancora post</li>
+                    @endforelse
+                </ul>
+            </div>
+        </div>
+    </div>
+
+
+@endsection
